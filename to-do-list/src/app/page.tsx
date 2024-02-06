4cc1c7222f4300cb8682fbd531c1350b627da4bf
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2024-02-06 14:28:30 +0100

    Update page.tsx

--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -2,7 +2,7 @@ import ADDTask from "./component/ADDTask";
 
 
 export default function Home() {
-  return (
+  return ( 
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
       <h1 className="text-2xl font-bold">To do List App</h1>
