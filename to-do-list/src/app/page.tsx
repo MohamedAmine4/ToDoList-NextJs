0c46049d32090245b51afc3bcb108459bee0f3a0
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2024-02-20 01:26:19 +0100

    :)

--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
-      <h1 className="text-2xl font-bold">To do List App</h1>
+      <h1 className="text-2xl font-bold">To-Do List by M.Amine 😊</h1>
       <ADDTask/>
       </div>
       <Todolist tasks={task}/>
